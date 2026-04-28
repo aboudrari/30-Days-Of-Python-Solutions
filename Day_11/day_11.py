c56a1de11af4832aba8f6dfc1b1203fbb2251b5c
aboudrari <aboudrariabdallah1@gmailcom>
2026-04-28 18:36:14 +0300

    Day 11 - Functions in progress

---
+++ Day_11/day_11.py
@@ -0,0 +1,82 @@
+import math
+def add_two_numbers (num1,num2):
+    return (num1 + num2)
+
+def area_of_circle (r):
+    pi = math.pi
+    return (pi * r**2)
+
+
+def add_all_nums(*numbers):
+    total = 0
+    for num in numbers :
+        if not int(num):
+            print("Should be a number")
+        else :
+            total += num 
+    return total
+
+def convert_celsius_to_fahrenheit(c):
+    # °F = (°C x 9/5) + 32
+    f = (c * 9/5 ) + 32
+    return f
+
+def check_season(month):
+    spring = ["March","April","May"]
+    Summer = ["June","July ",'August']
+    Autumn = ['September','October','November']
+    Winter = ['December','January','February']
+    if month in spring :
+        print("Spring")
+    elif month in Summer :
+        print("Summer")
+    elif month in Autumn :
+        print ("Autumn")
+    elif month in Winter :
+        print("Winter")    
+
+def calculate_slope(x1, y1, x2, y2):
+    return (y2 - y1) / (x2 - x1)
+
+
+def solve_quadratic_eqn(a, b, c):
+    discriminant = b**2 - 4*a*c
+
+    if discriminant > 0:
+        x1 = (-b + math.sqrt(discriminant)) / (2*a)
+        x2 = (-b - math.sqrt(discriminant)) / (2*a)
+        return (x1, x2)
+
+    elif discriminant == 0:
+        x = -b / (2*a)
+        return (x,)
+
+    else:
+        return ()
+    
+def print_list(lst):
+    for item in lst :
+        print(item)
+
+def reverse_list(arr):
+    return arr[::-1]
+
+# print(reverse_list([1, 2, 3, 4, 5]))
+# # [5, 4, 3, 2, 1]
+# print(reverse_list(["A", "B", "C"])) 
+# # ["C", "B", "A"]
+
+def capitalize_list_items(list_):
+    new_lst = []
+    for item in list_ :
+        new_lst.append(item.capitalized())
+    return new_lst
+
+def add_item(lst , item) :
+    lst.append(item)
+    return lst
+
+food_stuff = ['Potato', 'Tomato', 'Mango', 'Milk'];
+print(add_item(food_stuff, 'Meat'))     # ['Potato', 'Tomato', 'Mango', 'Milk','Meat'];
+numbers = [2, 3, 7, 9];
+print(add_item(numbers, 5))      # [2, 3, 7, 9, 5]
