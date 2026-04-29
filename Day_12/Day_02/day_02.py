d282a41d05470eb7572e9abbef03eff7e35753a9
aboudrari <aboudrariabdallah1@gmailcom>
2026-04-29 20:16:29 +0300

    Day 02 - Variables & Built-in Functions completed ✅

---
+++ Day_12/Day_02/day_02.py
@@ -0,0 +1,70 @@
+
+# Variables in Python
+print('Day 2: 30 Days of python programming')
+print()
+first_name = 'abdallah'
+last_name = 'aboudrari'
+full_name = 'Abdallah Aboudrari'
+country = 'Morocco'
+city = 'Belfaa'
+age = 23
+year = 2003
+is_married = False
+skills = ['Pandas', 'Numpy', 'ML', 'C#', 'Python']
+person_info = {
+    'firstname': 'Abdallah',
+    'lastname': 'Aboudrari',
+    'country': 'Morocco',
+    'city': 'Belfaa'
+}
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Abdallah', 'Aboudrari', 'Morocco', 23, False
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
+
+# data type 
+print("first name type :" ,type(first_name))
+print("lasst name type :" ,type(last_name))
+print("full name type :" ,type(full_name))
+print("country type :" ,type(country))
+print("city  name type :" ,type(city))
+print("age type :" ,type(age))
+print("year type :" ,type(year))
+print("I am married type :" ,type(is_married))
+print("skills type :" ,type(skills))
+print("person info type :" ,type(person_info))
+
+print("Length of my fist name :" , len(first_name))
+print(f"lenght of my first name : {first_name} , lenght of my last name : {last_name}")
+num_one = 5 
+num_two = 4
+varaible_total = num_one + num_two
+varaible_diff = num_one - num_two
+varaible_product = num_two * num_one
+varaible_division = num_one / num_two
+varaible_remainder = num_two % num_one
+varaible_exp = num_one ** num_two
+floor_division = num_one // num_two
+# find the area of the circle 
+radius = 30 
+
