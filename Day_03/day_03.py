6d2b7a44b9c603a30dc3295b3ad3538bef7e380c
aboudrari <aboudrariabdallah1@gmailcom>
2026-04-30 22:52:30 +0300

    Day 03 - Operators completed ✅

--- Day_03/day_03.py
+++ Day_03/day_03.py
@@ -7,3 +7,25 @@ base = input("Enter base : ")
 height = input("Enter height : ")
 area = 0.5 * int(base) * int(height)
 print("The area of the traingle is " ,area)
+
+#  Calculate the perimeter of the triangle (perimeter = a + b + c).
+side_a = input("Enter side a:")
+side_b = input("Enter side b:")
+side_c = input("Enter side c:")
+triangle = int(side_a) + int(side_b) + int(side_c)
+print("The perimeter of the triangle is " , triangle)
+
+# rectangle 
+lenght = input("Enter Length :")
+width = input("Enter width : ")
+area = int(lenght) * int(width)
+perimeter = 2 * (int(lenght) + int(width))
+print("The area of the rectangle is " , area)
+print("The perimeter of the rectange is " ,perimeter)
+
+#radius of a circle
+radius = input("Enter radius :")
+area = 3.14 * radius**2
+circumference = 2 * 3.14 * radius
+print("Area of a circle " , area)
+print("Circulference of circle is " , circumference)
