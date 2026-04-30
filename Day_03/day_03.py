1b0f630c94fc0f930ae4841aefcd6cc3ec1422b8
aboudrari <aboudrariabdallah1@gmailcom>
2026-04-30 20:38:04 +0300

    Day 03 - Operators in progress

---
+++ Day_03/day_03.py
@@ -0,0 +1,9 @@
+age = 23
+height = 1.82
+complex_number = 3 + 4j
+
+# area of this triangle (area = 0.5 x b x h).
+base = input("Enter base : ")
+height = input("Enter height : ")
+area = 0.5 * int(base) * int(height)
+print("The area of the traingle is " ,area)
